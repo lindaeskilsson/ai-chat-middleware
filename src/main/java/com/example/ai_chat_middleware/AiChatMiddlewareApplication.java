fefed1404335160610280a645d334cc13f5a2ecf
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-05-04 13:43:13 +0200

    initial commit

---
+++ src/main/java/com/example/ai_chat_middleware/AiChatMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_chat_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_chat_middleware/AiChatMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_chat_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
